09dfa03bbffafcced9751c4a2b2cf50e9b68f9a5
David <davidzucco22@gmail.com>
2018-09-18 15:14:36 -0400

    database connectivity

--- basketball-app/UserAuthViewController.swift
+++ basketball-app/UserAuthViewController.swift
@@ -14,31 +14,47 @@ class UserAuthViewController: UIViewController {
     
     @IBOutlet weak var loginEmail: UITextField!
     @IBOutlet weak var loginPass: UITextField!
-    
     @IBOutlet weak var registerEmail: UITextField!
-    @IBOutlet weak var registerUser: UITextField!
     @IBOutlet weak var registerPass: UITextField!
     
-    
-    @IBAction func loginClicked(_ sender: Any) {
-        var db: DatabaseReference!
-        var db_handler: DatabaseHandle!
-        db = Database.database().reference()
-        db.child("APM").setValue("Mike")
-        db_handler = db.child("APM").observe(.childAdded, with: { (data) in
-            let value : String = (data.value as? String)!
-            print(value)
-        })
-        Auth.auth().signIn(withEmail: loginEmail.text!, password: loginPass.text!) { (user, error) in
-            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Auth.auth().addStateDidChangeListener() { auth, user in
+            if user != nil {
+//                at this point, we know the user is authenticated, and can proceed to auth guarded app screens.
+//                self.performSegue(withIdentifier: self.viewController, sender: nil)
+                self.loginEmail.text = nil
+                self.loginPass.text = nil
+                self.registerEmail.text = nil
+                self.registerPass.text = nil
+            }
         }
     }
     
     @IBAction func registerClicked(_ sender: Any) {
         Auth.auth().createUser(withEmail: registerEmail.text!, password: registerPass.text!) { (authResult, error) in
-            guard let user = authResult?.user else { return }
-            
+            if error == nil {
+                guard let user = authResult?.user else { return }
+                let ref = Database.database().reference(withPath: "users")
+                let userRef = ref.child(user.uid)
+                let userData : [String: Any] = ["uid":  user.uid]
+                userRef.setValue(userData)
+            }
         }
     }
     
+    @IBAction func loginClicked(_ sender: Any) {
+        Auth.auth().signIn(withEmail: loginEmail.text!, password: loginPass.text!) { (user, error) in
+            if let error = error, user == nil {
+                let alert = UIAlertController(title: "Sign In Failed",
+                                              message: error.localizedDescription,
+                                              preferredStyle: .alert)
+
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 }
+
