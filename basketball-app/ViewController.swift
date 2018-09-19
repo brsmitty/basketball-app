858476f23592353389aa4f26091340d5c31b3721
Yiwei Zhang <maggie.zyw@gmail.com>
2018-09-19 10:33:11 -0400

    Adding import file view controller

--- basketball-app/ViewController.swift
+++ basketball-app/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
+        print("hello")
+    debugPrint("hello")
     }
 
     override func didReceiveMemoryWarning() {
