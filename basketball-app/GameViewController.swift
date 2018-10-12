187c1af09b0d69e454ee2beab2e5395b23888e70
David <davidzucco22@gmail.com>
2018-10-12 12:34:26 -0400

    gamify progress

--- basketball-app/GameViewController.swift
+++ basketball-app/GameViewController.swift
@@ -13,35 +13,57 @@ import FirebaseDatabase
 
 class GameViewController: UIViewController {
     
+    var panStartPoint = CGPoint() //coordinates of pan start
+    var panEndPoint = CGPoint() //coordinates of pan end
+    var boxHeight : CGFloat = 100.0
+    var boxWidth : CGFloat = 100.0
+    var boxRects : [CGRect] = [CGRect.init(), CGRect.init(), CGRect.init(), CGRect.init(), CGRect.init(), CGRect.init()] //[0] = hoop, [1] = PG, [2] = SG, [3] = SF, [4] = PF, [5] = C
+    
+    @IBOutlet weak var imageHoop: UIImageView!
+    @IBOutlet weak var imagePlayer1: UIImageView!
+    @IBOutlet weak var imagePlayer2: UIImageView!
+    @IBOutlet weak var imagePlayer3: UIImageView!
+    @IBOutlet weak var imagePlayer4: UIImageView!
+    @IBOutlet weak var imagePlayer5: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        boxRects[0] = CGRect.init(x: imageHoop.frame.origin.x, y: imageHoop.frame.origin.y, width: boxWidth, height: boxHeight)
+        boxRects[1] = CGRect.init(x: imagePlayer1.frame.origin.x, y: imagePlayer1.frame.origin.y, width: boxWidth, height: boxHeight)
+        boxRects[2] = CGRect.init(x: imagePlayer2.frame.origin.x, y: imagePlayer2.frame.origin.y, width: boxWidth, height: boxHeight)
+        boxRects[3] = CGRect.init(x: imagePlayer3.frame.origin.x, y: imagePlayer3.frame.origin.y, width: boxWidth, height: boxHeight)
+        boxRects[4] = CGRect.init(x: imagePlayer4.frame.origin.x, y: imagePlayer4.frame.origin.y, width: boxWidth, height: boxHeight)
+        boxRects[5] = CGRect.init(x: imagePlayer5.frame.origin.x, y: imagePlayer5.frame.origin.y, width: boxWidth, height: boxHeight)
     }
     
-    var initialCenter = CGPoint()
-    
     @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        
         guard recognizer.view != nil else {return}
-        let piece = recognizer.view!
-        // Get the changes in the X and Y directions relative to
-        // the superview's coordinate space.
-        let translation = recognizer.translation(in: piece.superview)
+        let player = recognizer.view!
+        let translation = recognizer.translation(in: player.superview)
+        
         if recognizer.state == .began {
-            print("pan begin")
-            // Save the view's original position.
-            self.initialCenter = piece.center
+            self.panStartPoint = player.center
+            print(determineBoxIndex(point: self.panStartPoint))
+            
         }
-        // Update the position for the .began, .changed, and .ended states
-        if recognizer.state != .cancelled {
-            print("pan changed/ended")
-            // Add the X and Y translation to the view's original position.
-            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
-            piece.center = newCenter
+        
+        if recognizer.state == .ended {
+            
+            self.panEndPoint = CGPoint(x: panStartPoint.x + translation.x, y: panStartPoint.y + translation.y)
+            print(determineBoxIndex(point: self.panEndPoint))
+            
         }
-        else {
-            print("pan cancelled")
-            // On cancellation, return the piece to its original location.
-            piece.center = initialCenter
+            
+        if recognizer.state == .cancelled {print("cancel")}
+    }
+    
+    func determineBoxIndex(point: CGPoint) -> Int {
+        var i: Int = 0
+        for rect in boxRects {
+            if rect.contains(point){ return i }
+            else{ i += 1 }
         }
+        return 999
     }
 }
