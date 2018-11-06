8ea4a2e50d94c4eb1df6155e64b8c884e1c68d0c
Mike White <Mike@Mikes-MacBook-Pro.local>
2018-11-06 14:31:03 -0500

    Performance view and Settings view

---
+++ basketball-app/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  basketball-app
+//
+//  Created by Mike White on 11/6/18.
+//  Copyright © 2018 David Zucco. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
