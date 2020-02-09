4b5661346d74c764dd49b25301fa3778b38385ed
Alex Jacobs <jacobs.979@osu.edu>
2020-02-08 20:48:09 -0500

    increasing the max height from 7 foot 2 to 7 foot 11 in the player manager

--- basketballApp/PlayerManager/PlayerManagerViewController.swift
+++ basketballApp/PlayerManager/PlayerManagerViewController.swift
@@ -86,7 +86,8 @@ class PlayerManagerViewController: UIViewController, UITableViewDataSource, UITa
    // Array of all position names for the picker wheel
    let positionNames:[String] = [String] (arrayLiteral: "Point-Guard", "Shooting-Guard", "Small-Forward", "Center", "Power-Forward")
    // Array of all height names for the picker wheel
-   let heights:[String] = [String] (arrayLiteral: "5'0\"","5'1\"","5'2\"","5'3\"","5'4\"","5'5\"","5'6\"","5'7\"","5'8\"","5'9\"","5'10\"","5'11\"","6'0\"","6'1\"","6'2\"","6'3\"","6'4\"","6'5\"","6'6\"","6'7\"","6'8\"","6'9\"","6'10\"","6'11\"","7'0\"","7'1\"","7'2\"")
+   let heights:[String] = [String] (arrayLiteral: "5'0\"","5'1\"","5'2\"","5'3\"","5'4\"","5'5\"","5'6\"","5'7\"","5'8\"","5'9\"","5'10\"","5'11\"","6'0\"","6'1\"","6'2\"","6'3\"","6'4\"","6'5\"","6'6\"","6'7\"","6'8\"","6'9\"","6'10\"","6'11\"","7'0\"","7'1\"","7'2\"","7'3\"","7'4\"","7'5\"","7'6\"","7'7\""
+        ,"7'8\"","7'9\"","7'10\"","7'11\"")
 //   let cellNames:[String] = [String] (arrayLiteral: "points","assists","steals","2pg","fg","drebound","3pg","ft%","deflections","orebound","ftmade","blocks","pfoul","tfoul","charge")
    // Array of all class rank names for the picker wheel
    let ranks:[String] = [String] (arrayLiteral: "Freshmen","Sophomore","Junior","Senior")
