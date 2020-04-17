fa5926c825687bd1baa31ab9397d17f82869019e
Alex Jacobs <jacobs.979@osu.edu>
2020-04-17 13:49:43 -0400

    got the back button working for the admin settings tab

---
+++ basketballApp/AdminSettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AdminSettingsViewController.swift
+//  basketballApp
+//
+//  Created by Alex Jacobs on 4/15/20.
+//  Copyright © 2020 David Zucco. All rights reserved.
+//
+
+import UIKit
+
+class AdminSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // for the back button on the admin settings page
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
