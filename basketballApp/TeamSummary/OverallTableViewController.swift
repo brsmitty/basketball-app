0ff4a8d77796c6a35116f3cccdfd64a3d7c45ab9
Kevin Oliver <oliver.395@osu.edu>
2021-03-18 11:23:00 -0400

    table values change color based on goals

--- basketballApp/TeamSummary/OverallTableViewController.swift
+++ basketballApp/TeamSummary/OverallTableViewController.swift
@@ -21,24 +21,80 @@ class OverallTableViewController: UITableViewController {
     @IBOutlet weak var oppOreb: UILabel!
     
     @IBOutlet weak var usFTs: UILabel!
+    //I think this is a misspelling but it breaks other stuff when I change it
     @IBOutlet weak var oppOts: UILabel!
+    
+    var usTOGoal: Int!
+    var usFGpercentGoal: Int!
+    var usFTGoal: Int!
+    var usOrebGoal: Int!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        usFTs.text = "22"
+        usFTs.sizeToFit()
+        oppOts.sizeToFit()
+        usFTGoal = 80
+        usFTs.text = "76"
+        let usFTnum = Int(usFTs.text!)
+        if usFTnum! > usFTGoal
+        {
+            usFTs.textColor = UIColor.red
+        }
+        if usFTnum! < usFTGoal
+        {
+            usFTs.textColor = UIColor.green
+        }
+        
         
         usTOs.sizeToFit()
         oppTOs.sizeToFit()
+        usTOs.text = "10"
+        usTOGoal = 12
+        let usTOnum = Int(usTOs.text!)
+        if usTOnum! > usTOGoal
+        {
+            usTOs.textColor = UIColor.red
+        }
+        if usTOnum! < usTOGoal
+        {
+            usTOs.textColor = UIColor.green
+        }
+        
+        
         
         usFGpercent.sizeToFit()
         oppFGpercent.sizeToFit()
+        usFGpercent.text = "61"
+        usFGpercentGoal = 55
+        let usFGpercentnum = Int(usFGpercent.text!)
+        if usFGpercentnum! > usFGpercentGoal
+        {
+            usFGpercent.textColor = UIColor.red
+        }
+        if usFGpercentnum! < usFGpercentGoal
+        {
+            usFGpercent.textColor = UIColor.green
+        }
+        
         
         usOreb.sizeToFit()
         oppOreb.sizeToFit()
+        usOreb.text = "12"
+        usOrebGoal = 10
+        let usOrebnum = Int(usOreb.text!)
+        if usOrebnum! > usOrebGoal
+        {
+            usOreb.textColor = UIColor.red
+        }
+        if usOrebnum! < usOrebGoal
+        {
+            usOreb.textColor = UIColor.green
+        }
         
-        usFTs.sizeToFit()
-        oppOts.sizeToFit()
+        
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
